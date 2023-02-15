2c1880bfa6f16671e7636ba9a9c389080802c3f6
Dmitry Kulikov <dima@koulikoff.ru>
2023-02-15 18:25:34 +0100

    AuthenticationHelper is added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,58 @@
 class ApplicationController < ActionController::Base
 
   helper :layout
+  include AuthenticationHelper
 
+  before_action :set_locale
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = t :access_denied
+    store_target_location
+    if current_user.new_record?
+      redirect_to login_url
+    else
+      redirect_to user_path(id: current_user.id)
+    end
+  end
+
+  def self.default_url_options(options = {})
+    options.merge(locale: I18n.locale)
+  end
+
+  protected
+
+  def current_ability
+    @current_ability ||= Ability.new current_user
+  end
+
+  private
+
+  def preferred_language
+    (user_preferred_languages & I18n.available_locales.map(&:to_s)).first || I18n.default_locale
+  end
+
+  def set_locale    # rubocop:disable Metrics/AbcSize
+    I18n.locale =
+      if params[:locale] and I18n.available_locales.include? params[:locale].to_sym
+        params[:locale]
+      else
+        preferred_language
+      end
+    logger.debug "ApplicationController@#{__LINE__}#set_locale locale is #{I18n.locale.inspect}" if logger.debug? # rubocop:disable Metrics/LineLength
+    logger.info "ApplicationController@#{__LINE__}#set_locale user is #{current_user&.name.presence || 'guest'}" if logger.info? # rubocop:disable Metrics/LineLength
+  end
+
+  def user_preferred_languages
+    request.headers['Accept-Language'].split(',').collect do |l|
+      l += ';q=1.0' unless l =~ /;q=\d+\.\d+$/
+      l.split(';q=')
+    end.sort do |x, y|
+      raise 'Not correctly formatted' unless x.first =~ /^[a-z\-]+$/i
+      y.last.to_f <=> x.last.to_f
+    end.collect do |l|
+      l.first.downcase.gsub(/-[a-z]+$/i) { |x| x.upcase }
+    end
+  rescue # Just rescue anything if the browser messed up badly.
+    []
+  end
 end
---
+++ app/helpers/authentication_helper.rb
@@ -0,0 +1,41 @@
+# This module is included in your application controller which makes
+# several methods available to all controllers and views. Here's a
+# common example you might add to your application layout file.
+#
+#   <% if logged_in? %>
+#     Welcome <%= current_user.username %>! Not you?
+#     <%= link_to "Log out", logout_path %>
+#   <% else %>
+#     <%= link_to "Sign up", signup_path %> or
+#     <%= link_to "log in", login_path %>.
+#   <% end %>
+#
+# You can also restrict unregistered users from accessing a controller using
+# a before filter. For example.
+#
+#   before_filter :login_required, :except => [:index, :show]
+module AuthenticationHelper
+
+  def current_user_session
+    # @current_user_session or @current_user_session = UserSession.find
+  end
+
+  def current_user
+    @current_user or @current_user = current_user_session&.record || User.new
+  end
+
+  def logged_in?
+    not current_user.new_record?
+  end
+
+  def redirect_to_target_or_default(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
+  end
+
+  private
+
+  def store_target_location
+    session[:return_to] = request.fullpath
+  end
+end

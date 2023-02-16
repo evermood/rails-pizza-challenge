1e5c93095a8c287bf5980485521c76d584e15a75
Dmitry Kulikov <dima@koulikoff.ru>
2023-02-16 17:40:32 +0100

    enable_extension 'pgcrypto'

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_16_140019) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_16_163834) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "discounts", id: false, force: :cascade do |t|
